fb730f2a1462513f4fd061807881382ab4cd10a3
hitch2 <hitch2@users.noreply.github.com>
2021-03-06 03:45:19 +0000

    deploy: 777086e1504ea964c4c6ea670ecc2c856f8acb1f

--- search/defines_c.js
+++ search/defines_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['trng_5fct_2060',['TRNG_CT',['../neorv32_8h.html#abfd8dc1811ee923d8d7ca2672304dcc4',1,'neorv32.h']]],
-  ['twi_5fct_2061',['TWI_CT',['../neorv32_8h.html#a89965ec91a0788976fb79821991f1fc3',1,'neorv32.h']]],
-  ['twi_5fdata_2062',['TWI_DATA',['../neorv32_8h.html#aaad9f06d6da950b43cc158cf2d28c92d',1,'neorv32.h']]]
+  ['trng_5fct_2064',['TRNG_CT',['../neorv32_8h.html#abfd8dc1811ee923d8d7ca2672304dcc4',1,'neorv32.h']]],
+  ['twi_5fct_2065',['TWI_CT',['../neorv32_8h.html#a89965ec91a0788976fb79821991f1fc3',1,'neorv32.h']]],
+  ['twi_5fdata_2066',['TWI_DATA',['../neorv32_8h.html#aaad9f06d6da950b43cc158cf2d28c92d',1,'neorv32.h']]]
 ];
--- search/defines_d.js
+++ search/defines_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['uart0_5fct_2063',['UART0_CT',['../neorv32_8h.html#a6ec3803d2b69338976b9633eeefaee5c',1,'neorv32.h']]],
-  ['uart0_5fdata_2064',['UART0_DATA',['../neorv32_8h.html#a804851f5c7db55ac6d32a96862ea326f',1,'neorv32.h']]],
-  ['uart1_5fct_2065',['UART1_CT',['../neorv32_8h.html#a7231bd478d6c08551f323650287e27ed',1,'neorv32.h']]],
-  ['uart1_5fdata_2066',['UART1_DATA',['../neorv32_8h.html#a487fe772b17de5e5453cb9616f06b964',1,'neorv32.h']]]
+  ['uart0_5fct_2067',['UART0_CT',['../neorv32_8h.html#a6ec3803d2b69338976b9633eeefaee5c',1,'neorv32.h']]],
+  ['uart0_5fdata_2068',['UART0_DATA',['../neorv32_8h.html#a804851f5c7db55ac6d32a96862ea326f',1,'neorv32.h']]],
+  ['uart1_5fct_2069',['UART1_CT',['../neorv32_8h.html#a7231bd478d6c08551f323650287e27ed',1,'neorv32.h']]],
+  ['uart1_5fdata_2070',['UART1_DATA',['../neorv32_8h.html#a487fe772b17de5e5453cb9616f06b964',1,'neorv32.h']]]
 ];
